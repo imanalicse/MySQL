d688e16eca6187158e82775d77c2c75828cac8c9
Iman <imanali.cse@gmail.com>
2021-09-22 10:49:37 +0600

    strict mode

---
+++ strict_mode.sql
@@ -0,0 +1,10 @@
+SET GLOBAL innodb_strict_mode=0;
+SET SESSION innodb_strict_mode=1;
+
+SET GLOBAL innodb_strict_mode=OFF;
+SET SESSION innodb_strict_mode=OFF;
+SHOW VARIABLES LIKE 'innodb_strict_mode';
+ 
+SET GLOBAL innodb_strict_mode=ON;
+SET SESSION innodb_strict_mode=ON;
+SHOW VARIABLES LIKE 'innodb_strict_mode';
