1e2c22cc6430c3e6c62381665647ecac315f46e2
Iman <imanali.cse@gmail.com>
2022-01-14 20:37:40 +0600

    command

--- comman_line.cmd
+++ comman_line.cmd
@@ -1,5 +1,4 @@
 database upload via cmd:
 
 mysql -u root -p database_name < C:\Users\user\Downloads\database_file.sql
-
-mysql -h localhost -u root webapp_doyour_events < C:\Users\user\Downloads\database_file.sql
+mysql -h localhost -u root database_name < C:\Users\user\Downloads\database_file.sql
