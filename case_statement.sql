06e575b0fdff936bc7930c2b0fec00c5e06325bd
Iman <imanali.cse@gmail.com>
2022-05-09 17:09:47 +0600

    case statement

---
+++ case_statement.sql
@@ -0,0 +1,16 @@
+/*
+ https://dev.mysql.com/doc/refman/5.7/en/case.html
+
+ CASE case_value
+    WHEN when_value THEN statement_list
+    [WHEN when_value THEN statement_list] ...
+    [ELSE statement_list]
+END CASE
+
+CASE
+    WHEN search_condition THEN statement_list
+    [WHEN search_condition THEN statement_list] ...
+    [ELSE statement_list]
+END CASE
+
+ */
