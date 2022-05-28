4d01318c591904c5295819ecc281799f1bb30ec3
Iman <imanali.cse@gmail.com>
2022-05-28 15:06:01 +0600

    alter table

--- alter_table.sql
+++ alter_table.sql
@@ -4,3 +4,19 @@
 ALTER TABLE table_name
   MODIFY COLUMN column_name_1 VARCHAR(100),
   MODIFY COLUMN column_name_2 VARCHAR(100);
+
+/*
+#To add two or more columns to a table at the same time, you use the following syntax:
+
+ALTER TABLE table
+ADD [COLUMN] column_name_1 column_1_definition [FIRST|AFTER existing_column],
+ADD [COLUMN] column_name_2 column_2_definition [FIRST|AFTER existing_column],
+...;
+*/
+
+ALTER TABLE shared_s3v3_users ADD COLUMN show_org_dashboard_overview TINYINT(2) NOT NULL DEFAULT 1;
+ALTER TABLE shared_s3v3_users ADD COLUMN profile_tour_completion_date DATETIME NULL DEFAULT NULL;
+
+
+
+
