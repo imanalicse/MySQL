37b8a51aa43df08442d1cace8c6d5b23521b3495
Iman <imanali.cse@gmail.com>
2022-07-17 15:55:10 +0600

    upcoming

--- dateTime.sql
+++ dateTime.sql
@@ -5,11 +5,11 @@ SELECT NOW(), UTC_TIMESTAMP(), DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(UTC_TIME
 
 
 #Upcomming
-SELECT id, organisation_id, campaign_name,is_pre_flight_check_result,`status`, total_recipient,scheduled_at,
-                DATE_FORMAT(FROM_UNIXTIME(scheduled_at), '%Y-%m-%d %H:%i:%s') AS scheduled_date_time
+SELECT id, campaign_name, scheduled_at, DATE_FORMAT(FROM_UNIXTIME(scheduled_at), '%Y-%m-%d %H:%i:%s') AS scheduled_date_time
                 FROM shared_s3v3_org_event_compaigns
-                WHERE DATE_FORMAT(FROM_UNIXTIME(scheduled_at), '%Y-%m-%d %H:%i:%s') BETWEEN (NOW() + INTERVAL 1 HOUR) AND (NOW() + INTERVAL 25 HOUR)
-                AND status = 'active' ORDER BY id;
+                WHERE DATE_FORMAT(FROM_UNIXTIME(scheduled_at), '%Y-%m-%d %H:%i:%s')
+                    BETWEEN (NOW() + INTERVAL 1 HOUR) AND (NOW() + INTERVAL 25 HOUR)
+                ORDER BY id;
 
 
 SELECT *
