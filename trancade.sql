0bfd16da084b300dcb3286af05a7591238975505
Iman <imanali.cse@gmail.com>
2022-09-08 16:24:42 +0600

    truncate

--- trancade.sql
+++ trancade.sql
@@ -1,1 +1,7 @@
 TRUNCATE TABLE table_name;
+
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE shared_s3v3_stage_image_unique_scheduler;
+TRUNCATE TABLE shared_s3v3_stage_images;
+TRUNCATE TABLE shared_s3v3_event_mapped_participants;
+SET FOREIGN_KEY_CHECKS = 1;
