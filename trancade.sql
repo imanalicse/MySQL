cd0fd5a22fedb739dadb26870039a16f08d6a63d
Iman <imanali.cse@gmail.com>
2022-10-16 12:03:47 +0600

    truncate

--- trancade.sql
+++ trancade.sql
@@ -1,5 +1,9 @@
 TRUNCATE TABLE table_name;
 
+/**
+How to truncate a foreign key constrained table?
+https://stackoverflow.com/questions/5452760/how-to-truncate-a-foreign-key-constrained-table
+*/
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE shared_s3v3_stage_image_unique_scheduler;
 TRUNCATE TABLE shared_s3v3_stage_images;
