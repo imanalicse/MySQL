f6d36dab04046499756319055ebc9a0c0d8332d5
Iman <imanali.cse@gmail.com>
2023-05-10 09:58:24 +0600

    index

--- Index/index.sql
+++ Index/index.sql
@@ -14,6 +14,7 @@ SHOW INDEX FROM shared_s3v3_payments;
 
 CREATE UNIQUE INDEX index_name  ON table_name(column_name);
 CREATE UNIQUE INDEX unique_payment_method_code  ON shared_s3v3_payments(payment_method_code);
+ALTER TABLE shared_s3v3_stage_images ADD CONSTRAINT unique_emp_id_and_img_name UNIQUE(emp_id, img_name);
 
 DROP INDEX index_name ON table_name
 DROP INDEX `PRIMARY` ON table_name;
