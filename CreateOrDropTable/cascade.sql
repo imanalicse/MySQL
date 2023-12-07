a36d2dc0990624c39c72e7e31de0cdd44568414c
Iman <imanali.cse@gmail.com>
2023-12-07 08:36:10 +0600

    cascading

--- CreateOrDropTable/cascade.sql
+++ CreateOrDropTable/cascade.sql
@@ -12,8 +12,11 @@ CREATE TABLE posts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (300),
     user_id INT NOT NULL,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT fk_users_posts_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+# fk_[referencing table name]_[referenced table name]_[referencing field name]
+# CONSTRAINT fk_users_favoritePosts_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 
 INSERT INTO users (first_name, last_name, email, password) VALUES ("Iman", "Ali", "imanali.cse@gmail.com", "123456");
 INSERT INTO posts (title, user_id) VALUES ("Post 1", 1);
