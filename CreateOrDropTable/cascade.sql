6848f1438147cdacf294af07a4c7660bf87bcad6
Iman <imanali.cse@gmail.com>
2023-12-07 10:55:07 +0600

    cascading

--- CreateOrDropTable/cascade.sql
+++ CreateOrDropTable/cascade.sql
@@ -12,14 +12,14 @@ CREATE TABLE posts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (300),
     user_id INT NOT NULL,
-    CONSTRAINT Fk_Users_Posts_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT Fk_Users_Posts_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 # fk_[referencing table name]_[referenced table name]_[referencing field name]
-# CONSTRAINT Fk_Users_FavoritePosts_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+# CONSTRAINT Fk_Users_FavoritePosts_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 
 # fk_[referencing table name]_[referencing field name]_[referenced table name]_[referenced field name]
-# CONSTRAINT Fk_Users_id_FavoritePosts_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+# CONSTRAINT Fk_Users_id_FavoritePosts_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 
 INSERT INTO users (first_name, last_name, email, password) VALUES ("Iman", "Ali", "imanali.cse@gmail.com", "123456");
 INSERT INTO posts (title, user_id) VALUES ("Post 1", 1);
