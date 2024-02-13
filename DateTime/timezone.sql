8868439935c54033060e0fd00e42c7572d0fe84f
iman <iman@bitmascot.com>
2024-02-14 00:06:49 +1100

    Timezone

--- DateTime/timezone.sql
+++ DateTime/timezone.sql
@@ -1,6 +1,9 @@
 select NOW();
 #CONVERT_TZ (dt, from_tz,to_tz)
 select CONVERT_TZ (NOW(), '+00:00','+10:00');
-SELECT @@GLOBAL.time_zone, @@SESSION.time_zone;
+SELECT @@global.time_zone, @@session.time_zone, @@system_time_zone;
+select timediff(now(),convert_tz(now(),@@session.time_zone,'+00:00'));
 
 DATE_FORMAT(FROM_UNIXTIME(1654049042), '%Y-%m-%d %H:%i:%s');
+
+SELECT UTC_TIMESTAMP(),CURRENT_TIMESTAMP(), NOW();
