46f016b4c4a5ded8f40f14cae79bef55e1243770
Iman <imanali.cse@gmail.com>
2024-02-17 20:03:13 +0600

    Duplicate

--- Duplicate.sql
+++ Duplicate.sql
@@ -6,6 +6,7 @@ SELECT student_id, cermdt_ds, cermtm, COUNT(*) group_count FROM federation_zo75n
 GROUP BY student_id, cermdt_ds, cermtm HAVING group_count > 1 ORDER BY cermdt_ds DESC;
 
 -- The following statement deletes duplicate rows and keeps the highest id: -- not tested
-DELETE t1 FROM shared_s3v3_stage_images t1
-                   INNER JOIN shared_s3v3_stage_images t2
-WHERE t1.id > t2.id AND  t1.img_name = t2.img_name;
+DELETE t1 FROM categories_copy t1
+                   INNER JOIN categories_copy t2
+                    ON t1.name = t2.name
+WHERE t1.id > t2.id;
