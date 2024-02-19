bce834b417f41dad8d010ba1fd604b5c3af2deb5
iman <iman@bitmascot.com>
2024-02-19 22:19:15 +1100

    Find in set

--- Funcations/Advanced/FIND_IN_SET.sql
+++ Funcations/Advanced/FIND_IN_SET.sql
@@ -1,2 +1,11 @@
 SELECT FIND_IN_SET('y','x,y,z'); -- 2
 -- continue
+/**
+  FIND_IN_SET() function used to find the position of a string within a list of strings.
+  If the string is repeated multiple time, the output will be the first position of that string.
+
+  Point to be noted :
+    If string is not found in string_list, the result is 0
+    If string or string_list is NULL, the result is NULL
+    If string_list is an empty string (“”), the result is 0
+*/
