4edc6903cbe668967f8152f3e67ad1b05dbcdd13
iman <iman@bitmascot.com>
2024-03-31 16:26:35 +1100

    video

--- rgs/video.sql
+++ rgs/video.sql
@@ -3,3 +3,6 @@ update shared_s3v3_student_stage_videos set is_published_to_rabbitmq = 0, is_vid
 where is_video_processed = 0 AND is_process_failed = 1 and created >= '2024-03-18';
 
 select * from shared_s3v3_student_stage_videos where is_video_processed = 0 AND is_process_failed = 1 and created LIKE '2024-03-29%';
+
+update shared_s3v3_student_stage_videos SET expired_at = '2024-03-31 04:51:42', deleted_at = NULL, is_process_failed = 0,
+is_video_processing = 0, is_published_to_rabbitmq = 0 WHERE id = 121;
