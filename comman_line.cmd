035d2279540bb81c71050fce4a7dec5806d87b8a
Iman <imanali.cse@gmail.com>
2024-08-03 16:50:27 +0600

    import database command

--- comman_line.cmd
+++ comman_line.cmd
@@ -2,3 +2,5 @@ database upload via cmd:
 
 mysql -u root -p database_name < C:\Users\user\Downloads\database_file.sql
 mysql -h localhost -u root database_name < C:\Users\user\Downloads\database_file.sql
+
+mysql -u root -p --port 3307 database_name < C:\Users\user\Downloads\database_name.sql
