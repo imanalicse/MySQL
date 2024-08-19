968f474bb296d3cd056b776818a4123cd42109d3
iman <iman@bitmascot.com>
2024-08-19 12:18:17 +1000

    digitalization

--- eb/currency.sql
+++ eb/currency.sql
@@ -2,3 +2,5 @@ SELECT currency FROM shared_s3v3_org_after_pay_payments WHERE reference_id = 'R5
 SELECT event_id, base_currency_code, currency_code FROM shared_s3v3_org_orders WHERE order_reference_code = 'R5M2B4-P30KT9-1723603470';
 
 SELECT id, currency FROM shared_s3v3_org_events WHERE id = (SELECT event_id FROM shared_s3v3_org_orders WHERE order_reference_code = 'R5M2B4-P30KT9-1723603470');
+
+SELECT currency FROM shared_s3v3_organisations WHERE id = (SELECT organisation_id FROM shared_s3v3_org_orders WHERE order_reference_code = 'R5M2B4-P30KT9-1723603470');
---
+++ rgs/digitalisation.sql
@@ -0,0 +1,2 @@
+SELECT * FROM une_utn16_students WHERE cermdt_ds = 240818 AND cermtm = 2300;
+update une_utn16_students SET cermdt_ds = 240819 WHERE cermdt_ds = 240818 AND cermtm = 2300;
