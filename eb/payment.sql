42a4057fcec3429a9cbcd6f811787fb67716a117
iman <iman@bitmascot.com>
2024-09-01 19:51:12 +1000

    eb payment

--- eb/payment.sql
+++ eb/payment.sql
@@ -12,3 +12,10 @@ SELECT * FROM shared_s3v3_org_license WHERE license_key LIKE '%.%' AND organisat
 
 
 SELECT id, country, country_code FROM shared_s3v3_organisations where `name` = 'AU org seeder one';
+
+
+SELECT id, payment_method, order_reference_code, payment_reference_number, event_payment_method FROM shared_s3v3_org_orders WHERE order_reference_code LIKE '%YGN1Z5%' ORDER BY id DESC LIMIT 5;
+SELECT * FROM shared_s3v3_org_securepay_payments WHERE refid LIKE '%YGN1Z5%';
+
+SELECT * FROM shared_s3v3_org_payment_methods WHERE payment_method = 'PayPal' AND organisation_id IN (SELECT id FROM shared_s3v3_organisations WHERE country = 'South Africa');
+DELETE FROM shared_s3v3_org_payment_methods WHERE payment_method = 'PayPal' AND organisation_id IN (SELECT id FROM shared_s3v3_organisations WHERE country = 'South Africa');
