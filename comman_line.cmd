452f304cec8d9d243d544a1368cd4eb1d32f1f42
iman <iman@bitmascot.com>
2024-09-09 15:12:12 +1000

    sql dump

--- comman_line.cmd
+++ comman_line.cmd
@@ -4,3 +4,5 @@ mysql -u root -p database_name < C:\Users\user\Downloads\database_file.sql
 mysql -h localhost -u root database_name < C:\Users\user\Downloads\database_file.sql
 
 mysql -u root -p --port 3307 database_name < C:\Users\user\Downloads\database_name.sql
+
+mysqldump -u root -p database_name > C:\Users\user\Downloads\database_name.sql
