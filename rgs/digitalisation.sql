6e8a447f14c450434723d071722d1756c0e1bdab
iman <iman@bitmascot.com>
2024-09-19 13:56:13 +1000

    rgs

--- rgs/digitalisation.sql
+++ rgs/digitalisation.sql
@@ -1,2 +1,3 @@
 SELECT * FROM une_utn16_students WHERE cermdt_ds = 240818 AND cermtm = 2300;
 update une_utn16_students SET cermdt_ds = 240819 WHERE cermdt_ds = 240818 AND cermtm = 2300;
+UPDATE unimelb_8949r_student_ceremonies SET cermdt_ds ='240819' WHERE cermdt_ds = 240818 AND cermtm = 2300;
