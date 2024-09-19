fda137f6e74ac1a3a034496afb0120896ea2e470
iman <iman@bitmascot.com>
2024-09-19 13:57:21 +1000

    rgs

--- rgs/student_ceremony_update.sql
+++ rgs/student_ceremony_update.sql
@@ -1,4 +1,4 @@
-UPDATE unimelb_8949r_students SET cermdt_ds ='240430' WHERE cermdt_ds = '240424';
-UPDATE unimelb_8949r_student_ceremonies SET cermdt_ds ='240430' WHERE cermdt_ds = '240424';
-SELECT id, cermdt_ds from unimelb_8949r_students WHERE cermdt_ds = '240424';
-SELECT id, cermdt_ds from unimelb_8949r_student_ceremonies WHERE cermdt_ds = '240424';
+UPDATE unimelb_8949r_students SET cermdt_ds ='240430' WHERE cermdt_ds = '240424' AND cermtm = 2300;
+UPDATE unimelb_8949r_student_ceremonies SET cermdt_ds ='240430' WHERE cermdt_ds = '240424' AND cermtm = 2300;
+SELECT id, cermdt_ds from unimelb_8949r_students WHERE cermdt_ds = '240424' AND cermtm = 2300;
+SELECT id, cermdt_ds from unimelb_8949r_student_ceremonies WHERE cermdt_ds = '240424' AND cermtm = 2300;
