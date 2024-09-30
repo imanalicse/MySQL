d46cdaa056c1053589894b84a9a9a988f9df49ad
iman <iman@bitmascot.com>
2024-09-30 23:08:57 +1000

    Video

--- RGS/video.sql
+++ RGS/video.sql
@@ -1,5 +1,7 @@
 -- For reprocess video
-update shared_s3v3_student_stage_videos set is_published_to_rabbitmq = 0, is_video_processing = 0, is_process_failed = 0, process_start_at = null, expired_at = null, deleted_at = null, is_saved_notification = 1, notification_save_error = ''
+update shared_s3v3_student_stage_videos set is_published_to_rabbitmq = 0, is_video_processing = 0, is_video_processed = 0,
+    is_process_failed = 0, process_start_at = null, processed_at = null, expired_at = null, deleted_at = null,
+    is_saved_notification = 0, notification_save_error = ''
 where is_video_processed = 0 AND is_process_failed = 1 and created LIKE '2024-09%';
 
 select * from shared_s3v3_student_stage_videos where is_video_processed = 0 AND is_process_failed = 1 and created LIKE '2024-09%';
