95efa58a85b5433815221c35d6f139ddb9cfb495
iman <iman@bitmascot.com>
2024-12-01 15:34:08 +1100

    RGS frame

--- RGS/degree_frame_rgs.sql
+++ RGS/degree_frame_rgs.sql
@@ -5,6 +5,9 @@ UPDATE latrobe_d8765_product_options SET `status` = 0 WHERE attribute_id = 57 AN
 UPDATE latrobe_d8765_product_options SET `status` = 0 WHERE attribute_id = 57 AND product_id = 19 AND option_id IN (SELECT id FROM latrobe_d8765_options WHERE attribute_id = 57 AND `mould` IN ('Black', 'Prestige'));
 
 #new query:
+SELECT id FROM latrobe_d8765_product_options WHERE product_id = 19 AND `status` = 1 AND option_id IN (SELECT id FROM latrobe_d8765_options WHERE colour = 'Black');
+SELECT id FROM latrobe_d8765_product_options WHERE product_id = 20 AND `status` = 1 AND option_id IN (SELECT id FROM latrobe_d8765_options WHERE colour = 'Black');
+
 SELECT id FROM latrobe_d8765_product_options WHERE product_id = 19 AND `status` = 1 AND option_id IN (SELECT id FROM latrobe_d8765_options WHERE mould = 'Black');
 SELECT id FROM latrobe_d8765_product_options WHERE product_id = 20 AND `status` = 1 AND option_id IN (SELECT id FROM latrobe_d8765_options WHERE mould = 'Black');
 
