436e2954afe71e0edff3ebd4be9203684195b3b0
iman <iman@bitmascot.com>
2025-02-15 17:04:15 +1100

    database design

--- DeleteData/delete.sql
+++ DeleteData/delete.sql
@@ -2,9 +2,7 @@ DELETE FROM table_name WHERE condition;
 DELETE FROM shared_s3v3_payments WHERE id = 22;
 
 -- delete the first three contacts sorted by first names
-DELETE FROM contacts
-ORDER BY first_name
-LIMIT 3;
+DELETE FROM contacts ORDER BY first_name LIMIT 3;
 
 -- delete all rows
 DELETE FROM contacts;
